8c595bcec5ae6ccbe41a4768e48318076337e550
ykim <ykim2718@gmail.com>
2025-12-10 10:35:35 -0600

    preventprevent_duplicate_topics()

--- bbPress settings.php
+++ bbPress settings.php
@@ -1,3 +1,26 @@
+// Y, 2025.12.6
+function prevent_duplicate_topics($topic_id) {
+    $topic_title = bbp_get_topic_title($topic_id);
+    $forum_id    = bbp_get_topic_forum_id($topic_id);
+
+    // 같은 포럼 내 동일 제목 검색
+    $args = array(
+        'post_type'   => bbp_get_topic_post_type(),
+        'post_parent' => $forum_id,
+        'title'       => $topic_title,
+        'post_status' => 'publish',
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->found_posts > 1) {
+        // 중복 발견 시 삭제
+        wp_delete_post($topic_id, true);
+        bbp_add_error('duplicate_topic', __('이미 동일한 제목의 토픽이 존재합니다.', 'bbpress'));
+    }
+}
+add_action('bbp_new_topic', 'prevent_duplicate_topics');
+
 // Y, 2025.12.7
 // Subscriber가 자신이 작성한 글만 삭제 가능
 function bbpress_subscriber_delete_own( $caps, $cap, $user_id, $args ) {
